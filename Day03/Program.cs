7034d53c281015d29d19c99244eb0836ba4c6358
JuniosSTL <junios@naver.com>
2017-09-02 14:27:32 +0900

    첫번째 함수
    Signed-off-by: JuniosSTL <junios@naver.com>

---
+++ Day03/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            string input = Console.ReadLine();
+            count = int.Parse(input);
+
+            for (int y = 0; y < count; ++y)
+            {
+                PrintLine(y);
+            }
+
+            for (int y = count; y >= 0; --y)
+            {
+                PrintLine(y);
+            }
+        }
+
+        static void PrintLine(int count)
+        {
+            for (int x = 0; x <= count; ++x)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
