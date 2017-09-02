567b0391e086d3ff9774da8e3ff0ee45ddff984f
DESKTOP-KCCM5P0\skilltreelab <hydrangea22@naver.com>
2017-09-02 16:10:39 +0900

    Call by Reference 오버로딩

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -10,13 +10,59 @@ namespace Day03
     {
         static void Main(string[] args)
         {
-            int number = Add3(5);
+            int a = 3;
+            int b = 5;
+            float c = 3.5f;
+            float d = 2.4f;
+
+            SwapNumber(ref a, ref b);
+            SwapNumber(a, b);
+            SwapNumber(c, d);
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
         }
 
-        static int Add3(int x)
+
+        //Call by Reference
+        static void SwapNumber(ref int a1, ref int b1)
         {
-            return x + 3;
+            int c = 0;
+
+            c = a1;     //main->a
+            a1 = b1;    //main->b
+            b1 = c;
+
+            Console.WriteLine(a1);
+            Console.WriteLine(b1);
         }
 
+
+        //Call by Value
+        static void SwapNumber(int a1, int b1)
+        {
+            int c = 0;
+
+            c = a1;     //main->a
+            a1 = b1;    //main->b
+            b1 = c;
+
+            Console.WriteLine(a1);
+            Console.WriteLine(b1);
+        }
+
+
+        //Call by Value (float)
+        static void SwapNumber(float a1, float b1)
+        {
+            float c = 0;
+
+            c = a1;     //main->a
+            a1 = b1;    //main->b
+            b1 = c;
+
+            Console.WriteLine(a1);
+            Console.WriteLine(b1);
+        }
     }
 }
